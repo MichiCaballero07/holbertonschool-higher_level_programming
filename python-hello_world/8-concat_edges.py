13b0f50aa4747613a52670953fc9a471c3afa536
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-05-30 16:30:52 -0500

    new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-cadena = f'{str[39:66]}{str[106:111]} {str[:6]}'
+cadena = f'{str[39:66]}{str[106:112]}{str[:6]}'
 print(cadena)
