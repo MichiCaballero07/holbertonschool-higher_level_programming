991ea8c3c8176dce4df413d0a5d06c6d7e1e94c8
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-06-01 10:19:45 -0500

    assign a random signed number to the variable number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+
+if number < 0:
+    last_digit = -1 * last_digit
 
 if number > 5:
-    print('Last digit of, {} and is greater than 5'.format(number))
+    print('Last digit of {} is {} and is greater than 5'
+          .format(number, last_digit))
 
 elif number == 0:
-    print('Last digit of, {} and is 0'.format(number))
+    print('Last digit of {} is {} and is 0'
+          .format(number, last_digit))
 
-elif number < 6:
-    print('Last digit of, {} and is less than 6 and not 0'.format(number))
+elif number < 6 and last_digit != 0:
+    print('Last digit of {} is {} and is less than 6 and not 0'
+          .format(number, last_digit))
