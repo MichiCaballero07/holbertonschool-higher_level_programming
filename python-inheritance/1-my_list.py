1389dee305302a27c6bb7c8ed70201c1c6729c78
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-06-21 16:35:01 -0500

    add new file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Documentation"""
+
+
+class MyList(list):
+    """class MyList"""
+    def print_sorted(self):
+        """public function print_sorted"""
+        print(sorted(self))
