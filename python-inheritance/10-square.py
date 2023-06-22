140475d7a61a630352280f77c453501c9cba4967
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-06-22 17:01:45 -0500

    add new file

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """ class impor"""
 
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-Rectangle = __import__('8-rectangle').Rectangle
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-
+    """
+    size must be private. No getter or setter
+    size must be a positive integer, validated by integer_validator
+    """
     def __init__(self, size):
         self.__size = size
         self.integer_validator("size", size)
