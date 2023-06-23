ecdd72b3d24e334f4b7d958f8a92cf5291360c2b
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-06-23 10:48:52 -0500

    add new file

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Import the json module"""
+    
+    
+import json
+
+
+def from_json_string(my_str):
+    """function that returns an object"""
+    return json.loads(my_str)
