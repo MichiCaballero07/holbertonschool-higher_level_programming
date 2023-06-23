6f24bdb6839e2fe4f39521aaad6c0b82f9490055
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-06-23 11:57:14 -0500

    add new file

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -7,11 +7,11 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         if attrs is None:
             return self.__dict__
-        
+
         json_dict = {}
         for attr in attrs:
             if hasattr(self, attr):
