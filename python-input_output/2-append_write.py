46085d38491274c502fa31774374195ac5682acc
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-06-23 13:08:30 -0500

    add new file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,9 +5,5 @@
 def append_write(filename="", text=""):
     """Open the file in append mode"""
     with open(filename, 'a', encoding="utf-8") as file:
-        start_pos = file.tell()
-        file.write(text)
-        end_pos = file.tell()
-        num_characters_added = end_pos - start_pos
-
-    return num_characters_added
+         file.write(text)
+         return len(text)
