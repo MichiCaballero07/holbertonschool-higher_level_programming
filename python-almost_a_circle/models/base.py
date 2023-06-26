2bac40c60352c805b18e1076026cf6d73e1f46ca
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-06-26 15:28:58 -0500

    pycodestiyle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,10 +5,10 @@
 class Base:
     '''construct'''
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         self.id = Base.__nb_objects
         if id is not None:
             self.id = id
-             
+
         Base.__nb_objects += 1
