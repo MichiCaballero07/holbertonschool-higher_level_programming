8bc5cfb8eb31b9d69c2cbe046ab9ecf04aeb89c1
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-06-27 11:50:05 -0500

    adition conditional

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -24,6 +24,10 @@ class Rectangle(Base):
     '''add value to width'''
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value <= 0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     '''Access to height'''
@@ -34,6 +38,10 @@ class Rectangle(Base):
     '''add value to height'''
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value <= 0:
+            raise ValueError('height must be > 0')
         self.__height = value
 
     '''Access to x'''
@@ -44,6 +52,10 @@ class Rectangle(Base):
     '''add value to x'''
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError('x must be an integer')
+        elif value < 0:
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     '''Access to y'''
@@ -54,4 +66,8 @@ class Rectangle(Base):
     '''add value to y'''
     @y.setter
     def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError('y must be an integer')
+        elif value < 0:
+            raise ValueError('y must be >= 0')
         self.__y = value
