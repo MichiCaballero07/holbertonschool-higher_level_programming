07c221c8ce44b60eacc007c3648d8dd704b1c2c4
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-06-27 11:58:02 -0500

    pycodestyle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -10,10 +10,10 @@ from models.base import Base
 class Rectangle(Base):
     '''Class Resctangle inherited from Base'''
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     '''Access to width'''
@@ -36,6 +36,7 @@ class Rectangle(Base):
         return self.__height
 
     '''add value to height'''
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -45,11 +46,13 @@ class Rectangle(Base):
         self.__height = value
 
     '''Access to x'''
+
     @property
     def x(self):
         return self.__x
 
     '''add value to x'''
+
     @x.setter
     def x(self, value):
         if not isinstance(value, int):
@@ -59,11 +62,13 @@ class Rectangle(Base):
         self.__x = value
 
     '''Access to y'''
+
     @property
     def y(self):
         return self.__y
 
     '''add value to y'''
+
     @y.setter
     def y(self, value):
         if not isinstance(value, int):
