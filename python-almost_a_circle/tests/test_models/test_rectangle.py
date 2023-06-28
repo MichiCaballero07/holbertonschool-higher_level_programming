c880e893a860addf0be3aca9c582dd939eb69ab2
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-06-28 15:03:14 -0500

    test

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,38 @@
+import unittest
+from models.rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+
+    def test_triangle_with_two_arguments(self):
+        r1 = Rectangle(1, 2)
+        
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        
+    def test_triangle_with_tree_arguments(self):
+        r2 = Rectangle(1, 2, 3)
+        
+        self.assertEqual(r2.width, 1)
+        self.assertEqual(r2.height, 2)
+        self.assertEqual(r2.x, 3)
+        
+    def test_triangle_with_four_arguments(self):
+        r3 = Rectangle(1, 2, 3, 4)
+        
+        self.assertEqual(r3.width, 1)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 3)
+        self.assertEqual(r3.y, 4)
+        
+    def test_triangle_with_five_arguments(self):
+        r4 = Rectangle(1, 2, 3, 4, 5)
+        
+        self.assertEqual(r4.width, 1)
+        self.assertEqual(r4.height, 2)
+        self.assertEqual(r4.x, 3)
+        self.assertEqual(r4.y, 4)
+        self.assertEqual(r4.id, 5)
+        
+    
+
+    
