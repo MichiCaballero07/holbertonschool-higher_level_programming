971869a05c5e488ac28b47deb2b502305d114e70
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-06-28 13:11:43 -0500

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,9 +9,11 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    '''Square'''
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    '''function'''
     def __str__(self):
         return "[Square] ({}) {}/{} - {}"\
             .format(self.id, self.x, self.y, self.width)
