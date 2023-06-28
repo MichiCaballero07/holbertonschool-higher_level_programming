7e07fa92e859380d5f5a3007cac9cf1c4b8504e1
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-06-28 15:04:09 -0500

    tests

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,28 @@
+import unittest
+import json
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    def test_automatic_id(self):
+        b1 = Base()
+
+        
+        self.assertEqual(b1.id, 1)
+        
+    def test_automatic_increment(self):
+        b2 = Base()
+        b3 = Base()
+        
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        
+    def test_saving_id(self):
+        b4 = Base(89)
+        
+        self.assertEqual(b4.id, 89)
+        
+    
+
+        
+if __name__ == "__main__":
+    unittest.main()
